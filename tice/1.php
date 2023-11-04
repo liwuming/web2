7382f2806988cdc657d9521961e12330850c3ded
liwuming <1459543371@qq.com>
2023-11-04 15:17:22 +0800

    121

---
+++ tice/1.php
@@ -0,0 +1,9 @@
+<?php
+$obj = array(
+  "ProtocolVer" => 1,
+  "CommandSequence" => 0,
+  "CommandID" => 5,
+  "EquipmentID" => "HB-05-2A82320001518133",
+  "LaunchTime" => 1699065213
+);
+die(json_encode($obj));
